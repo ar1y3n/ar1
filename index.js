b50dcc2c12d73cfb5ee1e20c84fa2fe8220e875b
ar1y3n <155617890+ar1y3n@users.noreply.github.com>
2025-04-01 02:10:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKVie')
+  res.send('Hello MCKV')
 })
 
 app.get('/courses', function (req, res) {
